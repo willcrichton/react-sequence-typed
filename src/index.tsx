5fba1ca384072da7338c8cf9349befb68d47c5c9
Will Crichton <wcrichto@cs.stanford.edu>
2020-07-13 21:55:40 +0000

    Memoize child creation to avoid stateful issues

--- src/index.tsx
+++ src/index.tsx
@@ -27,28 +27,40 @@ export class Sequence extends React.Component<SequenceProps, SequenceState> {
   render() {
     let Child = this.props.children[this.state.index];
     return <div>
-      <Child prev={this.prev.bind(this)} next={this.next.bind(this)} />
+      <Child key={this.state.index} prev={this.prev.bind(this)} next={this.next.bind(this)} />
     </div>
   }
 }
 
 export class AccumulatingSequence extends Sequence {
   state = { index: 0 }
+  children: JSX.Element[] = []
+
+  constructor(props: SequenceProps) {
+    super(props);
+    this.make_child(0);
+  }
+
+  make_child(i: number) {
+    if (i == this.children.length) {
+      let Child = this.props.children[i];
+      this.children.push(<Child key={i} prev={this.prev.bind(this)} next={this.next.bind(this)} />);
+    }
+  }
 
   next() {
-    this.setState({index: Math.min(this.state.index + 1, this.props.children.length - 1)})
+    let index = Math.min(this.state.index + 1, this.props.children.length - 1);
+    this.make_child(index);
+    this.setState({index})
   }
 
   prev() {
-    this.setState({index: Math.max(this.state.index - 1, 0)});
+    let index = Math.max(this.state.index - 1, 0);
+    this.setState({index});
   }
 
   render() {
-    let children =
-      this.props.children
-          .slice(0, this.state.index+1)
-          .map((Child) =>
-            <Child prev={this.prev.bind(this)} next={this.next.bind(this)} />);
+    let children = this.children.slice(0, this.state.index+1)
     return <>{children}</>;
   }
 }
