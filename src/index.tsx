f25f53eefca24f3ca5519f4f153fb1e1807aa1d8
Will Crichton <wcrichto@cs.stanford.edu>
2020-10-16 00:12:09 +0000

    Allow null inputs

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ export interface SequenceChildProps {
 }
 
 export interface SequenceProps {
-  children: ((props: SequenceChildProps) => JSX.Element)[]
+  children: (((props: SequenceChildProps) => JSX.Element) | null)[]
 }
 
 interface SequenceState {
@@ -15,9 +15,15 @@ interface SequenceState {
 
 export class Sequence extends React.Component<SequenceProps, SequenceState> {
   state = { index: 0 }
+  children_input: any[] = []
+
+  constructor(props: SequenceProps) {
+    super(props);
+    this.children_input = props.children.filter((c) => c !== null);
+  }
 
   next() {
-    this.setState({index: Math.min(this.state.index + 1, this.props.children.length - 1)})
+    this.setState({index: Math.min(this.state.index + 1, this.children_input.length - 1)})
   }
 
   prev() {
@@ -25,7 +31,7 @@ export class Sequence extends React.Component<SequenceProps, SequenceState> {
   }
 
   render() {
-    let Child = this.props.children[this.state.index];
+    let Child = this.children_input[this.state.index];
     return <div>
       <Child key={this.state.index} prev={this.prev.bind(this)} next={this.next.bind(this)} />
     </div>
@@ -43,13 +49,13 @@ export class AccumulatingSequence extends Sequence {
 
   make_child(i: number) {
     if (i == this.children.length) {
-      let Child = this.props.children[i];
+      let Child = this.children_input[i];
       this.children.push(<Child key={i} prev={this.prev.bind(this)} next={this.next.bind(this)} />);
     }
   }
 
   next() {
-    let index = Math.min(this.state.index + 1, this.props.children.length - 1);
+    let index = Math.min(this.state.index + 1, this.children_input.length - 1);
     this.make_child(index);
     this.setState({index})
   }
